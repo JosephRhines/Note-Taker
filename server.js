ce5df506eb58f9f898b5a627e850f40e012e5a2b
Joseph Rhines <jprhines@gmail.com>
2021-02-02 22:13:02 -0600

    I added a app.get and and app.post fuction and was able to save notes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var fs = require("fs");
 var express = require("express");
 var path = require("path");
-
+var notes = []
 var app = express();
-var port = 3000;
+var port = process.env.port || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,9 +11,34 @@ app.use(express.json());
 app.use(express.static("./develop/public"));
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/develop/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/develop/db/db.json"))
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "/develop/public/index.html"));
 });
+
+
+
+
+
+app.post("/api/notes", function(req, res) {
+    const userNote = req.body;
+    const data = fs.readFileSync(__dirname + "/develop/db/db.json");
+    const product = JSON.parse(data)
+    userNote.id = product.length + 1;
+    product.push(userNote)
+      fs.writeFile(__dirname + "/develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
+          if (err) throw err
+          console.log("success")
+      })  
+res.json(userNote);
+})
+
+app.listen(port, function() {
+    console.log("server listening on: http://localhost:" + port)
+})
