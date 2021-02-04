a67e2a3f28590891d8a3bb5bb3b2237e5c8740d7
Joseph Rhines <jprhines@gmail.com>
2021-02-03 21:00:37 -0600

    I updated all D's in my paths with a capitol D for the Develop folder

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 var fs = require("fs");
 var express = require("express");
 var path = require("path");
-var notes = []
 var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./develop/public"));
+app.use(express.static("./Develop/public"));
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/develop/db/db.json"))
+    res.sendFile(path.join(__dirname, "/Develop/db/db.json"))
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
 });
 
 
@@ -28,11 +27,11 @@ app.get("*", function(req, res) {
 
 app.post("/api/notes", function(req, res) {
     const userNote = req.body;
-    const data = fs.readFileSync(__dirname + "/develop/db/db.json");
+    const data = fs.readFileSync(__dirname + "/Develop/db/db.json");
     const product = JSON.parse(data)
     userNote.id = product.length + 1;
     product.push(userNote)
-      fs.writeFile(__dirname + "/develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
+      fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
           if (err) throw err
           
       })  
@@ -43,7 +42,7 @@ res.json(userNote);
 
 app.delete("/api/notes/:id", function(req, res) {
     const remove = req.params.id;
-    const data = fs.readFileSync(__dirname + "/develop/db/db.json",);
+    const data = fs.readFileSync(__dirname + "/Develop/db/db.json",);
     const product =JSON.parse(data)
     
     for(let i = 0; i < product.length; i++) {
@@ -56,7 +55,7 @@ app.delete("/api/notes/:id", function(req, res) {
     
     }
 
-    fs.writeFile(__dirname + "/develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
+    fs.writeFile(__dirname + "/Develop/db/db.json", JSON.stringify(product), "utf-8", function(err) {
         if (err) throw err
         
     })
